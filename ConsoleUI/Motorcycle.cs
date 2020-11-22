3faa976c544f8657358ad493c4fbf7bd9f8f4850
Cameron-Stock <clstock33@gmail.com>
2020-11-21 19:42:28 -0600

    Init Commit

---
+++ ConsoleUI/Motorcycle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ConsoleUI
+{
+    public class Motorcycle : Vehicle
+    {
+        public Motorcycle()
+        {
+        }
+
+        public override void DriveAbstract()
+        {
+            Console.WriteLine($"This motorocycle is in drive!");
+        }
+
+        public bool HasSideCart { get; set; } = true;
+
+        public override void DriveVirtual()
+        {
+            Console.WriteLine($"This {GetType().Name} is a {GetType().BaseType.Name} that is virtually in drive!");
+        }
+    }
+}
---
+++ ConsoleUI/Vehicles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ConsoleUI
+{
+    public abstract class Vehicle
+    {
+        public int Year { get; set; } = 2019;
+        public string Make { get; set; } = "Default Make";
+        public string Model { get; set; } = "Default Model";
+
+        public abstract void DriveAbstract();
+
+        public virtual void DriveVirtual()
+        {
+            Console.WriteLine($"This {GetType().Name} is in drive!");
+        }
+
+    }
+}
